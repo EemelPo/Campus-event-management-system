9287fc5e489182a449d908315d870ee2852256f3
Joonas <Joonas.Ronimus@metropolia.fi>
2025-02-11 20:14:05 +0200

    fixed first commit

--- src/main/java/project/CreateEventController.java
+++ src/main/java/project/CreateEventController.java
@@ -1,4 +1,70 @@
 package project;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.time.LocalDate;
+
 public class CreateEventController {
+    @FXML
+    private DatePicker eventDatePicker;
+    @FXML
+    private TextField eventNameField;
+    @FXML
+    private TextField eventStartField;
+    @FXML
+    private TextField eventEndField;
+    @FXML
+    private TextField eventCategoryField;
+    @FXML
+    private TextField eventLocationField;
+    @FXML
+    private TextField eventDescriptionField;
+    @FXML
+    private Button saveEventButton;
+
+
+    private Event event;
+
+    @FXML
+
+    private void initialize(){
+        saveEventButton.setOnAction(event -> saveEvent());
+
+    }
+
+    private void saveEvent(){
+        String eventName = eventNameField.getText();
+        String eventStartTime = eventStartField.getText();
+        String eventEndTime = eventEndField.getText();
+        String eventCategory = eventCategoryField.getText();
+        String eventLocation = eventLocationField.getText();
+        String eventDescription = eventDescriptionField.getText();
+        LocalDate eventDate = eventDatePicker.getValue();
+        if (eventName.isEmpty() || eventStartTime.isEmpty() || eventEndTime.isEmpty() ||
+                eventCategory.isEmpty() || eventLocation.isEmpty() || eventDescription.isEmpty() ||
+                eventDate == null) {
+            Alert errorMessageAlert = new Alert(Alert.AlertType.ERROR);
+            errorMessageAlert.setContentText("Please fill all fields!");
+            errorMessageAlert.showAndWait();
+
+
+            return;
+        }
+
+
+        event = new Event(eventName, eventStartTime, eventEndTime, eventCategory, eventLocation, eventDescription, eventDate);
+
+        eventNameField.clear();
+        eventStartField.clear();
+        eventEndField.clear();
+        eventCategoryField.clear();
+        eventLocationField.clear();
+        eventDescriptionField.clear();
+        eventDatePicker.setValue(null);
+
+    }
+
+
 }
+
